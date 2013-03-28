81af577465f68a3c56d280cb9df7bf8a3a41b8ab
Edvinas Krucas <edv.krucas@gmail.com>
2013-03-28 10:21:46 +0200

    NEW: Added basic Role and RoleProvider implementation

---
+++ src/EdvinasKrucas/RBAuth/Implementations/Role.php
@@ -0,0 +1,49 @@
+<?php namespace EdvinasKrucas\RBAuth\Implementations;
+
+use EdvinasKrucas\RBAuth\Contracts\RoleInterface;
+use EdvinasKrucas\RBAuth\Contracts\RoleProviderInterface;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model implements RoleInterface, RoleProviderInterface
+{
+    /**
+     * Table to store roles.
+     *
+     * @var string
+     */
+    protected $table = 'roles';
+
+    /**
+     * Determines if a role has access to given permission.
+     *
+     * @param $identifier
+     * @return bool
+     */
+    public function can($identifier)
+    {
+        // TODO: Implement can() method.
+        return true;
+    }
+
+    /**
+     * Returns role name.
+     *
+     * @return string
+     */
+    public function getRoleName()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Returns role by its name.
+     *
+     * @param $roleName
+     * @return EdvinasKrucas\RBAuth\Contracts\RoleInterface
+     */
+    public function getByName($roleName)
+    {
+        return static::where('role', '=', $roleName)->first();
+    }
+
+}

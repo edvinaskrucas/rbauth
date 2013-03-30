f9fe8561404a5f193b51228135a62acc12a5ffd9
edvinaskrucas <edv.krucas@gmail.com>
2013-03-30 19:50:35 +0200

    Removed PermissionInterface and PermissionProviderInterface

--- src/Krucas/RBAuth/Implementations/Eloquent/Permission.php
+++ src/Krucas/RBAuth/Implementations/Eloquent/Permission.php
@@ -1,10 +1,8 @@
 <?php namespace Krucas\RBAuth\Implementations\Eloquent;
 
 use Illuminate\Database\Eloquent\Model;
-use Krucas\RBAuth\Contracts\PermissionInterface;
-use Krucas\RBAuth\Contracts\PermissionProviderInterface;
 
-class Permission extends Model implements PermissionInterface, PermissionProviderInterface
+class Permission extends Model
 {
     /**
      * Table to store permissions.
@@ -12,26 +10,4 @@ class Permission extends Model implements PermissionInterface, PermissionProvide
      * @var string
      */
     protected $table = 'permissions';
-
-    /**
-     * Returns value on which be checking for permission.
-     *
-     * @return mixed
-     */
-    public function getIdentifier()
-    {
-        return $this->permission;
-    }
-
-    /**
-     * Returns permission based on given identifier.
-     *
-     * @param $identifier
-     * @return \Krucas\RBAuth\Contracts\PermissionInterface|null
-     */
-    public function getByIdentifier($identifier)
-    {
-        return static::where('permission', $identifier)->first();
-    }
-
 }
